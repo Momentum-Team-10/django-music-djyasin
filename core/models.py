c1eacc5581eaa1a6cce7c4be4384070300ec0882
Lila <djyasin.ly@gmail.com>
2021-11-05 15:49:24 -0400

    Got the basic layout of my models.py set up.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Album(models.Model):
+
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+   
+
+    def created_at(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
