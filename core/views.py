d9eada3543a1729ddcac2e92884d32f3b88d97a2
Lila <djyasin.ly@gmail.com>
2021-11-06 17:18:34 -0400

    Added html templates for when I eventually get that far. Still can't figure out my path issue.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Album
 
 
 def album_list(request):
     albums = Album.objects.all()
-    return render(request, "core/templates/album_list.html",
+    return render(request, "templates/album_list.html",
                   {"albums": albums})
+
+
