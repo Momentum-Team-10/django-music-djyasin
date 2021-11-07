8e2b370c265099dd6325b12805f4ed37115faa8f
Lila <djyasin.ly@gmail.com>
2021-11-07 17:04:05 -0500

    Got albums rendering to page. Now understand context variables.

--- core/views.py
+++ core/views.py
@@ -5,4 +5,5 @@ from .models import Album
 def album_list(request):
     albums = Album.objects.all()
     return render(request, "albums/album_list.html",
-                  {"albums": albums})
+                {"albums": albums})
+
